aa2a868b93039f9ed6b2e72bb79f26515de6a2fa
Kioudji <kentin.steinmetz@laposte.net>
2014-07-07 23:41:14 +0200

    J'aime les nems.

--- UltimatePenguinRampage/src/UPR.cpp
+++ UltimatePenguinRampage/src/UPR.cpp
@@ -2,50 +2,52 @@
 #include <SDL.h>
 #include "UPR.h"
 
-//Dimensions de l'écran
+//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-bool SDLInit(SDL_Window*, SDL_Surface*);
-void SDLQuit(SDL_Window*);
+int main( int argc, char* args[] )
+{
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
 
-int main(int argc, char* args[]) {
-	SDL_Window* window = NULL; // Fenêtre du programme
-	SDL_Surface* screenSurface = NULL; // La surface contenue dans la fenêtre
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
 
-	if(!SDLInit(window, screenSurface)) {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-	}
-    screenSurface = SDL_GetWindowSurface(window); // Récupération de la surface à partir de la fenêtre créée
-    SDL_Delay( 2000 );
-
-    SDLQuit(window);
-
-	return 0;
-}
-
-// Initialise la SDL et retourne true si il n'y a pas d'erreur
-bool SDLInit(SDL_Window *window, SDL_Surface *screenSurface) {
-    SDL_DestroyWindow(window);
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) // Initialise la SDL
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-		return false;
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else
 	{
-		// Créé la fenêtre
+		//Create window
 		window = SDL_CreateWindow( "Ultimate Penguin Rampage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
-			return false;
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface( window );
+
+			//Fill the surface white
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+			//Update the surface
+			SDL_UpdateWindowSurface( window );
+
+			//Wait two seconds
+			SDL_Delay( 2000 );
 		}
 	}
-	return true;
-}
 
-// Quite la SDL proprement
-void SDLQuit(SDL_Window *window) {
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	//Destroy window
+	SDL_DestroyWindow( window );
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
 }
