ee8f98495f01f7551ae869e5361242f17e11659c
Kioudji <kentin.steinmetz@laposte.net>
2014-07-07 23:57:24 +0200

    G chanG la Klass person'âge, poseyy.

--- UltimatePenguinRampage/include/Personnage.h
+++ UltimatePenguinRampage/include/Personnage.h
@@ -6,8 +6,15 @@ class Personnage
 {
     public:
         Personnage();
+        void subitDegats(int nbDegats);
+        void utiliserArme();
     protected:
     private:
+        int m_pdv;
+        int m_mana;
+        int m_posX;
+        int m_posY;
+        bool m_vivant;
 };
 
 #endif // PERSONNAGE_H
