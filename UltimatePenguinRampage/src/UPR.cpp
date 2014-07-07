7844a90735c96fe1c482004b92c24f97898c2470
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2014-07-08 00:14:07 +0200

    ON A UN PINGOUIN!!

--- UltimatePenguinRampage/src/UPR.cpp
+++ UltimatePenguinRampage/src/UPR.cpp
@@ -6,29 +6,37 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-bool SDLInit(SDL_Window*, SDL_Surface*);
-void SDLQuit(SDL_Window*);
+SDL_Window *window = NULL;
+SDL_Surface *screenSurface = NULL;
+SDL_Surface *penguin = NULL;
+
+bool SDLInit();
+void SDLQuit();
 
 int main(int argc, char* args[]) {
-	SDL_Window* window = NULL; // Fenêtre du programme
-	SDL_Surface* screenSurface = NULL; // La surface contenue dans la fenêtre
 
-	if(!SDLInit(window, screenSurface)) {
+	if(!SDLInit()) {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
 	}
-    screenSurface = SDL_GetWindowSurface(window); // Récupération de la surface à partir de la fenêtre créée
-    SDL_Delay( 2000 );
-    printf("plop");
 
-    SDLQuit(window);
+	penguin = SDL_LoadBMP("img/PBface.bmp");
+	if(penguin == NULL) {
+        printf("plop");
+	}
+
+	SDL_BlitSurface(penguin, NULL, screenSurface, NULL);
+	SDL_UpdateWindowSurface(window);
+
+    SDL_Delay( 5000 );
+
+    SDLQuit();
 
 	return 0;
 }
 
 // Initialise la SDL et retourne true si il n'y a pas d'erreur
-bool SDLInit(SDL_Window *window, SDL_Surface *screenSurface) {
-    SDL_DestroyWindow(window);
+bool SDLInit() {
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) // Initialise la SDL
 	{
 		return false;
@@ -40,13 +48,16 @@ bool SDLInit(SDL_Window *window, SDL_Surface *screenSurface) {
 		if( window == NULL )
 		{
 			return false;
+		} else {
+		    screenSurface = SDL_GetWindowSurface(window); // Récupération de la surface à partir de la fenêtre créée
 		}
 	}
 	return true;
 }
 
 // Quite la SDL proprement
-void SDLQuit(SDL_Window *window) {
-    SDL_DestroyWindow(window);
+void SDLQuit() {
+    SDL_FreeSurface(penguin); penguin = NULL;
+    SDL_DestroyWindow(window); window = NULL;
     SDL_Quit();
 }
