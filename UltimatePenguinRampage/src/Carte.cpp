b26eb93ff1b3dbdffc2a1cc9c74b560a130d9826
Ragnarok9 <j.legaludec@gmail.com>
2014-07-08 11:27:11 +0200

    Debut de chargeur de carte

--- UltimatePenguinRampage/src/Carte.cpp
+++ UltimatePenguinRampage/src/Carte.cpp
@@ -1,8 +1,22 @@
 #include "Carte.h"
 #include <fstream>
+#include <iostream>
 #include <string>
 
-Carte::Carte()
-{
+using namespace std;
 
+Carte::Carte() {
+
+}
+
+void Carte::extraire(string m_nomFichier) {
+    ifstream fichier("map/1.txt"); //extraction du fichier
+    string ligne;
+
+    if(fichier) {
+            while(getline(fichier, ligne)) { //lit le fichier tant qu'il n'est pas finit
+                cout << ligne << endl;
+            }
+
+    }
 }
