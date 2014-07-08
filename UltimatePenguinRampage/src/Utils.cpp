c6a10758125165935143a2a1c773193af11bb2cc
Ragnarok9 <j.legaludec@gmail.com>
2014-07-08 15:26:59 +0200

    ON A UN PINGOUIN TRANSPARENT!!!!!!!!!

--- UltimatePenguinRampage/src/Utils.cpp
+++ UltimatePenguinRampage/src/Utils.cpp
@@ -4,6 +4,7 @@
 SDL_Surface* Utils::loadBMP(std::string path, SDL_Surface* &ecran) {
     SDL_Surface* retour = NULL;
     SDL_Surface* chargee = SDL_LoadBMP(path.c_str());
+    SDL_SetColorKey(chargee, SDL_TRUE, SDL_MapRGB(chargee->format, 255, 0, 255)); //Transparence
     if(chargee == NULL) {
         std::cout << "Erreur : L'image " << path << " n'a pas pu être chargée." << std::endl;
         return NULL;
