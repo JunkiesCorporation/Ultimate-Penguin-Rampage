ac838718a0244080c0f5af05446b21357a3668cb
Kioudji <kentin.steinmetz@laposte.net>
2014-07-08 12:18:45 +0200

    Création des bases du header de Ennemi

--- UltimatePenguinRampage/include/Ennemi.h
+++ UltimatePenguinRampage/include/Ennemi.h
@@ -1,10 +1,16 @@
 #ifndef ENNEMI_H
 #define ENNEMI_H
 
+#include <iostream>
+#include <string>
+#include "Personnage.h"
 
-class Ennemi
+
+
+class Ennemi : public Personnage
 {
     public:
+        void suisJoueur(int posJoueur[2]);
     protected:
     private:
 
