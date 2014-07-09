48f4845e6b99b8f32fa02a33c26d1c6499119fda
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2014-07-09 14:23:39 +0200

    Commentaires et restructuration
    Ajout de commentaires.
    Certaines variables renommées.

--- UltimatePenguinRampage/src/UPR.cpp
+++ UltimatePenguinRampage/src/UPR.cpp
@@ -11,20 +11,20 @@ bool SDLInit(SDL_Window**, SDL_Surface**);
 void SDLQuit(SDL_Window**);
 
 int main(int argc, char* args[]) {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
+    SDL_Window* fenetre = NULL; // Pointeur vers la fenêtre du programme
+    SDL_Surface* ecran = NULL; // Pointeur vers le contenu de la fenêtre
 
     // Initialisation de la SDL
-	if(!SDLInit(&window, &screenSurface)) {
+	if(!SDLInit(&fenetre, &ecran)) {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
 	}
 
     // Boucle principale du programme
     bool quit(false);
-    SDL_Event e;
+    SDL_Event e; // Objet permettant de gérer les événements
 
-    Jeu jeu(window, screenSurface);
+    Jeu jeu(fenetre, ecran); // Instanciation d'un objet Jeu. C'est là-dedans que le jeu se déroule
 
     while(!quit) {
         // Affichage et gestion du menu principal
@@ -36,13 +36,13 @@ int main(int argc, char* args[]) {
         }
     }
 
-    SDLQuit(&window);
+    SDLQuit(&fenetre);
 
 	return 0;
 }
 
 // Initialise la SDL et retourne true si il n'y a pas d'erreur
-bool SDLInit(SDL_Window** window, SDL_Surface** screenSurface) {
+bool SDLInit(SDL_Window** fenetre, SDL_Surface** ecran) {
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) // Initialise la SDL
 	{
 		return false;
@@ -50,19 +50,19 @@ bool SDLInit(SDL_Window** window, SDL_Surface** screenSurface) {
 	else
 	{
 		// Créé la fenêtre
-		*window = SDL_CreateWindow( "Ultimate Penguin Rampage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if(*window == NULL )
+		*fenetre = SDL_CreateWindow( "Ultimate Penguin Rampage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if(*fenetre == NULL )
 		{
 			return false;
 		} else {
-		    *screenSurface = SDL_GetWindowSurface(*window); // Récupération de la surface à partir de la fenêtre créée
+		    *ecran = SDL_GetWindowSurface(*fenetre); // Récupération de la surface à partir de la fenêtre créée
 		}
 	}
 	return true;
 }
 
 // Quite la SDL proprement
-void SDLQuit(SDL_Window** window) {
-    SDL_DestroyWindow(*window); *window = NULL;
+void SDLQuit(SDL_Window** fenetre) {
+    SDL_DestroyWindow(*fenetre); *fenetre = NULL;
     SDL_Quit();
 }
