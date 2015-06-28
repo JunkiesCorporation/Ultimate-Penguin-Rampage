9f2756537144aeb42fb2c9ac67a2c5168d611fe9
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-06-28 10:30:21 +0200

    Fenêtre du jeu agrandie

--- UltimatePenguinRampage/include/Constantes.h
+++ UltimatePenguinRampage/include/Constantes.h
@@ -5,8 +5,8 @@ const int LARGEUR_TILE(32); ///< Largeur d'une tile
 
 const int HAUTEUR_TILE(32); ///< Hauteur d'une tile
 
-const int LARGEUR_ECRAN(640); ///< Largeur de l'écran
+const int LARGEUR_ECRAN(800); ///< Largeur de l'écran = 25 tiles
 
-const int HAUTEUR_ECRAN(480); ///< Hauteur de l'écran
+const int HAUTEUR_ECRAN(640); ///< Hauteur de l'écran = 20 tiles
 
 #endif // CONSTANTES_H_INCLUDED
