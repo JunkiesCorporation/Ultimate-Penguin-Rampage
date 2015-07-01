6e50f3f38ed022a1577b0b650580f78b3bd07129
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-01 17:34:16 +0200

    Ajout de la classe Profil
    Ajout de "Profil.cpp" et "Profil.h". Cette classe servira à lancer le
    Jeu et ce dernier l'utilisera afin d'enregistrer les progrès du joueur.
    On pourra créer un nouveau Profil, l'enregistrer dans un de trois
    emplacements et le charger pour reprendre la partie là où elle avait été
    laissée.

---
+++ UltimatePenguinRampage_rebuild/include/Profil.h
@@ -0,0 +1,55 @@
+﻿#ifndef PROFIL_H
+#define PROFIL_H
+
+#include <string>
+
+/** Classe du profil du joueur.
+ * \todo description de Profil.
+ */
+class Profil
+{
+	public:
+		// Constructeurs
+		//-----------------------------
+		/** Constructeur par défaut.
+		 * Initialise les attributs membres de Profil à des valeurs par défaut.
+		 */
+		Profil();
+		
+		/** Constructeur permettant la configuration d'un profil.
+		 * @param p_premiere_fois Si il s'agit d'un nouveau profil ou non. Détermine si le constructeur créé un nouveau profil ou en un charge un déjà existant.
+		 */
+		Profil(bool p_premiere_fois);
+		//-----------------------------
+		
+		// Destructeur
+		//-----------------------------
+		/** Destructeur par défaut.
+		 */
+		~Profil();
+		//-----------------------------
+		
+		// Fonctions membres publiques
+		//-----------------------------
+		//-----------------------------
+	
+	protected:
+	
+	private:
+		// Attributs membres privés.
+		//-----------------------------
+		/** Numéro de l'emplacement de sauvegarde de ce profil.
+		 */
+		int emplacement_sauvegarde;
+		
+		/** Le nom du profil.
+		 */
+		std::string nom_du_profil;
+		
+		/** S'il s'agit de la première fois que ce profil est utilisé.
+		 */
+		bool premiere_fois;
+		//-----------------------------
+};
+
+#endif // PROFIL_H
---
+++ UltimatePenguinRampage_rebuild/src/Profil.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#include "Profil.h"
+#include "Texture.h"
+
+/* Constructeur par défaut.*/
+Profil::Profil()
+{
+	premiere_fois = true;
+	emplacement_sauvegarde = 0;
+	nom_du_profil = "Debug";
+	
+	std::cout << "Profil cree." << std::endl;
+}
+
+/* Constructeur permettant la configuration d'un profil.*/
+Profil::Profil(bool p_premiere_fois)
+{
+	// Si c'est la première fois, création d'un nouveau profil avec choix d'un emplacement de sauvegarde.
+	if(p_premiere_fois)
+	{
+		
+	}
+	// Sinon, chargement d'un profil existant après la sélection d'un emplacement de sauvegarde.
+	else
+	{
+		
+	}
+}
+
+/* Destructeur par défaut.*/
+Profil::~Profil()
+{
+	std::cout << "Profil " << nom_du_profil << " detruit." << std::endl;
+}
