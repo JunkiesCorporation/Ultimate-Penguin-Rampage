aa3310a006d6966398e4198494e95bf1475bc869
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-02 18:09:59 +0200

    Ajout du fichier contenant les Structures
    Contient la structure Position qui contient deux int : la coordonnée x
    et la coordonnée y.

---
+++ UltimatePenguinRampage_rebuild/include/Struct.h
@@ -0,0 +1,17 @@
+﻿#ifndef STRUCT_H
+#define STRUCT_H
+
+/** Petite structure pour contenir des positions avec 2 coordonnées.
+ */
+struct Position
+{
+	/** La coordonnée x de la position.
+	 */
+	int x;
+	
+	/** La coordonnée y de la position.
+	 */
+	int y;
+};
+
+#endif // STRUCT_H
