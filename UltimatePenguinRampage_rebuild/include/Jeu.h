538fc9a7c4194b328c7702d49c8d650414840934
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-02 18:50:26 +0200

    Création de la classe Jeu
    - Création de la classe Jeu qui régit le jeu au-delà du menu principal.
    
    Pour le moment, la fonction lancer(Profil*) affiche "l'écran principal
    du jeu" qui offre le choix du "mode histoire". Dans le futur, c'est ici
    que l'on pourra également choisir le "mode arène", le retour au menu
    principal, de sauvegarder le profil en cours d'utilisation, etc.

---
+++ UltimatePenguinRampage_rebuild/include/Jeu.h
@@ -0,0 +1,61 @@
+﻿#ifndef JEU_H
+#define JEU_H
+
+#include "Profil.h"
+#include "Texture.h"
+
+/** La classe qui contient le déroulement du jeu une fois lancé à partir du menu principal.
+ * \todo Documentation de Jeu.
+ * \todo Choisir le constructeur pour initialiser le Jeu.
+ */
+class Jeu
+{
+	public:
+		// Constructeurs
+		//-----------------------------
+		/** Constructeur par défaut.
+		 * Appelé lors de la création d'une instance de Jeu.
+		 * 
+		 * Initialise les attributs du Jeu avec des valeurs par défaut.
+		 */
+		Jeu();
+		//-----------------------------
+		
+		// Destructeur
+		//-----------------------------
+		/** Destructeur par défaut.
+		 */
+		~Jeu();
+		//-----------------------------
+		
+		// Fonctions membres publiques
+		//-----------------------------
+		/** Lance le jeu avec les données du Profil donné.
+		 *
+		 * @param profil_joueur Le profil utilisé par le joueur.
+		 */
+		void lancer(Profil* profil_joueur);
+		//-----------------------------
+	
+	protected:
+	
+	private:
+		// Fonctions membres privées
+		//-----------------------------
+		void chargerTexturesEcran();
+		
+		void libererTexturesEcran();
+		//-----------------------------
+		
+		// Attributs membres privés
+		//-----------------------------
+		Texture* m_texture_cadre_selection;
+		
+		Texture* m_texture_fond_jeu;
+		
+		Texture* m_texture_mode_histoire;
+		//-----------------------------
+	
+};
+
+#endif // JEU_H
---
+++ UltimatePenguinRampage_rebuild/src/Jeu.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+
+#include "Jeu.h"
+#include "Struct.h"
+#include "Timer.h"
+#include "UPR.h"
+
+/* Constructeur par défaut.*/
+Jeu::Jeu()
+{
+	// Réglage des pointeurs vers les Textures à NULL.
+	m_texture_cadre_selection = NULL;
+	m_texture_fond_jeu = NULL;
+	m_texture_mode_histoire = NULL;
+	
+	// temp
+	std::cout << "Jeu instancie." << std::endl;
+}
+
+/* Destructeur par défaut.*/
+Jeu::~Jeu()
+{
+	// Suppression des textures qui auraient été oubliées.
+	delete m_texture_cadre_selection;
+	delete m_texture_fond_jeu;
+	delete m_texture_mode_histoire;
+	
+	// Fermeture des pointeurs.
+	m_texture_cadre_selection = NULL;
+	m_texture_fond_jeu = NULL;
+	m_texture_mode_histoire = NULL;
+	
+	// temp
+	std::cout << "Jeu detruit." << std::endl;
+}
+
+/* Lance le jeu avec les données du Profil donné.*/
+void Jeu::lancer(Profil* profil_joueur)
+{
+	// Contrôle de la boucle du Jeu.
+	bool quit = false;
+	
+	// Nombre d'images de l'écran principal du jeu : 2
+	// Positions des images de l'écran principal du jeu.
+	Position positions_textures_ecran[2] = {{0, 0}};
+	
+	// Positions du curseur lors de l'écran principal du jeu.
+	Position positions_curseur_ecran[1] = {{0, 0}};
+	
+	// Élément de manipulations des événements.
+	SDL_Event e;
+	
+	// Timer pour bloquer les FPS à 60 et nombre ticks par image.
+	Timer timer;
+	int ticks_image = 0;
+	
+	//---------------------------------
+	
+	// temp
+	std::cout << "Jeu lance avec le profil : " << profil_joueur->getNomProfil() << std::endl;
+	
+	// Chargement des images de l'écran principal du jeu.
+	chargerTexturesEcran();
+	
+	// Attribution des positions des images de l'écran principal du jeu.
+	positions_textures_ecran[0] = {0, 0}; // m_texture_fond_jeu
+	positions_textures_ecran[1] = {120, 250}; // m_texture_mode_histoire
+	
+	// Attribution des positions du curseur lors de l'écran principal du jeu.
+	positions_curseur_ecran[0] = {115, 245}; // sur m_texture_mode_histoire
+	
+	// Boucle du jeu.
+	
+	while(!quit)
+	{
+		// Début du chronométrage de l'image.
+		timer.start();
+		
+		// Nettoyage du renderer.
+		SDL_RenderClear(UPR::renderer_SDL);
+		
+		// Affichage des images fixes de l'écran principal du jeu.
+		m_texture_fond_jeu->render(positions_textures_ecran[0]);
+		m_texture_mode_histoire->render(positions_textures_ecran[1]);
+		
+		// Affichage du curseur lors de l'écran principal du jeu.
+		m_texture_cadre_selection->render(positions_curseur_ecran[0]);
+		
+		// Affichage à l'écran.
+		SDL_RenderPresent(UPR::renderer_SDL);
+		
+		while(SDL_PollEvent(&e) != 0)
+		{
+			// Si une touche est appuyée.
+			if(e.type == SDL_KEYDOWN)
+			{
+				// Switch sur le symbole de la touche appuyée.
+				switch(e.key.keysym.sym)
+				{
+				// La touche "q" est appuyée.
+				case SDLK_q:
+					quit = true;
+					break;
+				}
+			}
+		}
+		
+		// Correction si le FPS dépasse le maximum.
+		ticks_image = timer.getTicks();
+		if(ticks_image < UPR::TICKS_ECRAN_PAR_IMAGE)
+		{
+			SDL_Delay(UPR::TICKS_ECRAN_PAR_IMAGE - ticks_image);
+		}
+	}
+	
+	
+	libererTexturesEcran();
+	
+	// temp
+	std::cout << "Jeu termine, retour au menu principal." << std::endl << std::endl;
+}
+
+void Jeu::chargerTexturesEcran()
+{
+	char chemin_image_cadre_selection[] = "img/jeu/jeu_cadre_selection.bmp";
+	char chemin_image_fond_jeu[] = "img/jeu/jeu_image_fond.bmp";
+	char chemin_image_mode_histoire[] = "img/jeu/jeu_mode_histoire.bmp";
+	
+	m_texture_cadre_selection = new Texture(chemin_image_cadre_selection);
+	m_texture_fond_jeu = new Texture(chemin_image_fond_jeu);
+	m_texture_mode_histoire = new Texture(chemin_image_mode_histoire);
+}
+
+void Jeu::libererTexturesEcran()
+{
+	delete m_texture_cadre_selection;
+	delete m_texture_fond_jeu;
+	delete m_texture_mode_histoire;
+	
+	m_texture_cadre_selection = NULL;
+	m_texture_fond_jeu = NULL;
+	m_texture_mode_histoire = NULL;
+}
