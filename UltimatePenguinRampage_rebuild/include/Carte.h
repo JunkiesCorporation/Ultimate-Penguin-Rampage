772f2a93b3eaacca4f0577f2399f1069326915bb
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-04 09:54:22 +0200

    Début d'insertion de la classe Carte d'avant.
    - Travail en cours. Pas encore d'ajout de la source au Makefile ni de
    compilation possible.

---
+++ UltimatePenguinRampage_rebuild/include/Carte.h
@@ -0,0 +1,51 @@
+﻿#ifndef CARTE_H
+#define CARTE_H
+
+/** Classe contenant toutes les informations sur la carte d'une arène.
+ */
+class Carte
+{
+	public:
+	// Constructeurs
+	//---------------------------------
+	/** Constructeur avec chemin d'accès au fichier contenant les données de la carte.
+	 * 
+	 * @param chemin_fichier Le chemin du fichier à charger.
+	 */
+	Carte(std::string chemin_fichier);
+	//---------------------------------
+	
+	// Destructeur
+	//---------------------------------
+	/** Destructeur par défaut.
+	 */
+	~Carte();
+	//---------------------------------
+	
+	// Fonctions membres publiques
+	//---------------------------------
+	//---------------------------------
+	
+	protected:
+	
+	private:
+	// Fonctions membres privées
+	//---------------------------------
+	/** Lis le fichier associé à la carte et charge les informations.
+	 *
+	 * @param chemin_fichier Le chemin du fichier à charger.
+	 */
+	void charger(std::string chemin_fichier);
+	
+	/** Libère les éléments chargés par #charger().
+	 */
+	void liberer();
+	//---------------------------------
+	
+	// Attributs privés
+	//---------------------------------
+	//---------------------------------
+	
+};
+
+#endif // CARTE_H
---
+++ UltimatePenguinRampage_rebuild/src/Carte.cpp
@@ -0,0 +1,54 @@
+﻿#include <fstream>
+
+#include "Carte.h"
+
+// Constructeurs
+//---------------------------------
+/* Constructeur avec chemin d'accès au fichier contenant les données de la carte.*/
+Carte::Carte(std::string chemin_fichier)
+{
+	// Initialisation des attributs à des valeurs par défaut.
+	// Aucun attribut à initialiser.
+	
+	// Chargement des informations de la carte à partir du fichier donné.
+	charger(chemin_fichier);
+}
+//---------------------------------
+
+// Destructeur
+//---------------------------------
+/* Destructeur par défaut.*/
+Carte::~Carte()
+{
+	// Aucun attribut à détruire.
+}
+//---------------------------------
+
+// Fonctions membres publiques
+//---------------------------------
+//---------------------------------
+
+// Fonctions membres privées
+//---------------------------------
+/* Lis le fichier associé à la carte et charge les informations.*/
+void Carte::charger(std::string chemin_fichier)
+{
+	// Chargement du fichier donné dans un flux d'entrée.
+	std::ifstream fichier(chemin_fichier.c_str());
+	
+	// Ligne de lecture du fichier.
+	std::string ligne = "";
+	
+	//---------------------------------
+	// Démarrage du chargement de la carte.
+	
+	if(fichier)
+	{
+		
+	}
+	else
+	{
+		
+	}
+}
+//---------------------------------
