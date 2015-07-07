c971297e329f102a148eb404eba91f31ed677617
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-07 11:08:04 +0200

    Suppression du mode plein écran
    - Modifications pour que le programme se lance dans une fenêtre plutôt
    que dans un écran.
    - Le problème d'affichage provenait effectivement du mode plein écran.
    - Le mode fenêtre permet également de visualiser la console pendant que
    le programme tourne.

--- UltimatePenguinRampage_rebuild/src/Utils.cpp
+++ UltimatePenguinRampage_rebuild/src/Utils.cpp
@@ -22,7 +22,7 @@ bool Utils::initialisationSDL()
 	else
 	{
 		// Tentative de création de la fenêtre.
-		UPR::fenetre_SDL = SDL_CreateWindow("Ultimate Penguin Rampage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP);
+		UPR::fenetre_SDL = SDL_CreateWindow("Ultimate Penguin Rampage", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, UPR::LARGEUR_ECRAN, UPR::HAUTEUR_ECRAN, SDL_WINDOW_SHOWN);
 		
 		// Gestion d'un échec potentiel.
 		if(UPR::fenetre_SDL == NULL)
@@ -33,7 +33,7 @@ bool Utils::initialisationSDL()
 		else
 		{
 			// Tentative de création du renderer.
-			UPR::renderer_SDL = SDL_CreateRenderer(UPR::fenetre_SDL, -1, SDL_RENDERER_ACCELERATED);
+			UPR::renderer_SDL = SDL_CreateRenderer(UPR::fenetre_SDL, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 			
 			// Gestion d'un échec potentiel.
 			if(UPR::renderer_SDL == NULL)
