e40ffd25bdcad0d28e8e1c5816cbc129d613fdf4
Cyprien de Saint Guilhem <cypriendsg@yahoo.fr>
2015-07-16 22:37:19 +0200

    Ajoute les fondations des ennemis
    Ajoute le squelette de base pour la classe Ennemi qui hérite de la
    classe Entité. C'est à dire un constructeur par défaut qui appelle
    celui d'Entité, un destructeur et une implémentaion de la méthode
    update(Carte) avec un corps vide.

---
+++ UltimatePenguinRampage/include/Ennemi.h
@@ -0,0 +1,49 @@
+﻿#ifndef ENNEMI_H
+#define ENNEMI_H
+
+#include "Entite.h"
+
+/** Classe qui gère les ennemis.
+ */
+class Ennemi : public Entite
+{
+	public:
+		// Constructeurs
+		//-----------------------------
+		/** Constructeur par défaut.
+		 *
+		 * Initialise les attributs à des valeurs par défaut.
+		 *
+		 * @sa Entite::Entite().
+		 */
+		Ennemi();
+		//-----------------------------
+		
+		// Destructeur
+		//-----------------------------
+		/** Destructeur par défaut.
+		 *
+		 * Supprime tout les attributs non nuls.
+		 */
+		~Ennemi();
+		//-----------------------------
+		
+		// Fonctions membres publiques
+		//-----------------------------
+		/** Met à jour l'ennemi.
+		 *
+		 * @sa Entite::update().
+		 */
+		void update(Carte const &carte);
+		//-----------------------------
+		
+	private:
+		// Types privés
+		//-----------------------------
+		/** Mot-clef permettant l'accès rapide aux méthodes d'Entite.
+		 */
+		typedef Entite super;
+		//-----------------------------
+};
+
+#endif // ENNEMI_H
---
+++ UltimatePenguinRampage/src/Ennemi.cpp
@@ -0,0 +1,30 @@
+﻿#include "Entite.h"
+
+// Constructeurs
+//-------------------------------------
+/* Constructeur par défaut.*/
+Ennemi::Ennemi() : super()
+{
+	// Aucun attributs à initialiser.
+}
+//-------------------------------------
+
+
+// Destructeur
+//-------------------------------------
+/* Destructeur par défaut.*/
+Ennemi::~Ennemi()
+{
+	// Aucun attributs à détruire.
+}
+//-------------------------------------
+
+
+// Fonctions membres publiques
+//-------------------------------------
+/* Met à jour l'ennemi.*/
+void Entite::update(Carte const &carte)
+{
+	/** @todo La mise à jour des ennemis.*/
+}
+//-------------------------------------
